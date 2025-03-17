0237019b80bd1cffc548bf32065619c07c4cb0b6
Developer-Zahid <coderexpertzahid@gmail.com>
2025-03-17 17:33:20 +0600

    Added slide out animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,23 @@
-function slideInAnimations() {
+function slideInAnimations(slides, startDelay = 300) {
     anime({
-        targets: '.swiper-slide',
+        targets: slides,
         opacity: [0, 1],
         scale: [0.98, 1],
         translateX: ['10px', '0px'],
         translateY: ['50px', '0px'],
-        delay: anime.stagger(80, {start: 100}),
+        delay: anime.stagger(80, {start: startDelay}),
+        easing: 'cubicBezier(.215,.61,.355,1)',
+    });
+}
+
+function slideOutAnimations(slides) {
+    anime({
+        targets: slides,
+        opacity: [1, 0],
+        scale: [1, 0.98],
+        translateX: ['0px', '-10px'],
+        translateY: ['0px', '-50px'],
+        delay: anime.stagger(80, {start: 0}),
         easing: 'cubicBezier(.215,.61,.355,1)',
     });
 }
@@ -13,22 +25,24 @@ function slideInAnimations() {
 
 $(document).ready(function () {
 
+    anime.set('[data-slider-filter]:not(.active) .swiper-slide', {
+        opacity: 0
+    });
+
     let swiper = new Swiper('[data-swiper="Filter-Slider"]', {
         slidesPerView: "auto",
         spaceBetween: 30,
         grabCursor: true,
-        observer: true,
         on: {
             init: function () {
-                slideInAnimations()
+                slideInAnimations($('[data-slider-filter].active').get(0).querySelectorAll('.swiper-slide'), 100);                
             },
-            update: function () {
-                slideInAnimations()
+            update: function (swiper) {
+                slideInAnimations(swiper.el.querySelectorAll('.swiper-slide'))
             },
         }
-
     });
-
+    
     $('[data-filter]').on('click', function(){
         $(this).siblings('[data-filter]').removeClass('active');
         $(this).addClass('active');
@@ -36,6 +50,7 @@ $(document).ready(function () {
         $(`[data-slider-filter="${$(this).data('filter')}"]`).addClass('active');
         swiper[$('[data-filter].active').index()].update();
         swiper[$('[data-filter].active').index()].slideTo(0, 0);
+        slideOutAnimations(swiper[$('[data-filter]:not(.active)').index()].el.querySelectorAll('.swiper-slide'))        
     })
     
     
