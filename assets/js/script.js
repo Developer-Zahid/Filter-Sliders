bdb817035d8d5fc62a2c4738d127a125e275638d
Developer-Zahid <coderexpertzahid@gmail.com>
2025-03-18 13:33:10 +0600

    Added navigation and updated design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-function slideInAnimations(slides, startDelay = 300) {
+/* Utility Functions */
+function slideInAnimations(slides, startDelay = 280) {
     anime({
         targets: slides,
         opacity: [0, 1],
@@ -9,36 +10,52 @@ function slideInAnimations(slides, startDelay = 300) {
         easing: 'cubicBezier(.215,.61,.355,1)',
     });
 }
-
-function slideOutAnimations(slides) {
-    anime({
-        targets: slides,
-        opacity: [1, 0],
-        scale: [1, 0.98],
-        translateX: ['0px', '-10px'],
-        translateY: ['0px', '-50px'],
-        delay: anime.stagger(80, {start: 0}),
-        easing: 'cubicBezier(.215,.61,.355,1)',
+function indicatorUpdate(){
+    $('[data-filter-list]').each(function(){
+        const activeElement = $(this).find('[data-filter].active').get(0);
+        const activeElementInfo = activeElement.getBoundingClientRect();
+        
+        $(this).css({
+            "--_indicator-width": `${activeElementInfo.width}px`,
+            "--_indicator-height": `${activeElementInfo.height}px`,
+            "--_indicator-position-top": `${activeElement.offsetTop}px`,
+            "--_indicator-position-left": `${activeElement.offsetLeft}px`
+        });
     });
+};
+function handleNavigationUpdateEvents(swiper){
+    if(swiper.isBeginning) {
+        $('[data-swiper-navigation="prev"]').prop('disabled', true);
+    } else if(swiper.isEnd) {
+        $('[data-swiper-navigation="next"]').prop('disabled', true);
+    } else {
+        $('[data-swiper-navigation]').removeAttr('disabled');
+    }
 }
 
-
 $(document).ready(function () {
-
-    anime.set('[data-slider-filter]:not(.active) .swiper-slide', {
-        opacity: 0
-    });
-
-    let swiper = new Swiper('[data-swiper="Filter-Slider"]', {
+    const swiperSlider = new Swiper('[data-swiper="Filter-Slider"]', {
         slidesPerView: "auto",
         spaceBetween: 30,
         grabCursor: true,
         on: {
-            init: function () {
-                slideInAnimations($('[data-slider-filter].active').get(0).querySelectorAll('.swiper-slide'), 100);                
+            init: function() {
+                $('[data-swiper-navigation="prev"]').prop('disabled', true)
+                slideInAnimations($('[data-slider-filter].active').get(0).querySelectorAll('.swiper-slide'), 100);
             },
-            update: function (swiper) {
-                slideInAnimations(swiper.el.querySelectorAll('.swiper-slide'))
+            update: function(swiper) {
+                slideInAnimations(swiper.el.querySelectorAll('.swiper-slide'));
+                $('[data-swiper-navigation="prev"]').prop('disabled', true);
+                $('[data-swiper-navigation="next"]').removeAttr('disabled');
+            },
+            resize: indicatorUpdate,
+            slideChange: handleNavigationUpdateEvents,
+            sliderMove: handleNavigationUpdateEvents,
+            reachBeginning: function() {
+                $('[data-swiper-navigation="prev"]').prop('disabled', true);
+            },
+            reachEnd: function() {
+                $('[data-swiper-navigation="next"]').prop('disabled', true);
             },
         }
     });
@@ -46,12 +63,22 @@ $(document).ready(function () {
     $('[data-filter]').on('click', function(){
         $(this).siblings('[data-filter]').removeClass('active');
         $(this).addClass('active');
+        indicatorUpdate();
         $('[data-slider-filter]').siblings('[data-slider-filter]').removeClass('active');
         $(`[data-slider-filter="${$(this).data('filter')}"]`).addClass('active');
-        swiper[$('[data-filter].active').index()].update();
-        swiper[$('[data-filter].active').index()].slideTo(0, 0);
-        slideOutAnimations(swiper[$('[data-filter]:not(.active)').index()].el.querySelectorAll('.swiper-slide'))        
-    })
-    
-    
+        swiperSlider[$('[data-filter].active').index()].update();
+        swiperSlider[$('[data-filter].active').index()].slideTo(0, 0);
+    });
+
+    $('[data-swiper-navigation]').on('click', function(){
+        if($(this).data('swiper-navigation') == 'prev') {
+            swiperSlider[$('[data-filter].active').index()].slidePrev()
+        } else {
+            swiperSlider[$('[data-filter].active').index()].slideNext()
+        }
+    });
+
+    setTimeout(()=>{
+        indicatorUpdate();
+    }, 100);
 });
